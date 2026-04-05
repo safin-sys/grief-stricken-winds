26f1cb0ccf31fc7a833bbcea55ed7fd4ac84dc3b
safin-sys <mdsafinofficial@gmail.com>
2026-04-05 21:03:23 +0600

    initial setup

--- src/consts.ts
+++ src/consts.ts
@@ -3,19 +3,16 @@
 
 import type { Multilingual } from "@/i18n";
 
-export const SITE_TITLE: string | Multilingual = "Astro i18n Starter";
+export const SITE_TITLE: string | Multilingual = "Grief-Stricken Winds";
 
 export const SITE_DESCRIPTION: string | Multilingual = {
-	en: "A starter template for Astro with i18n support.",
-	ja: "i18n 対応の Astro スターターテンプレート。",
-	"zh-cn": "具有 i18n 支持的 Astro 入门模板。",
-	ar: "قالب بداية لـ Astro مع دعم i18n.",
+	en: "A docuseries on the genocide of the Eelam Tamils being carried out by the Sri Lankan state",
+	bn: "শ্রীলঙ্কার দ্বারা ইলাম তামিলদের ওপর চালানো গণহত্যার ওপর একটি তথ্যচিত্র"
 };
-
-export const X_ACCOUNT: string | Multilingual = "@psephopaiktes";
 
 export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
 	en: "This page is not available in your language.",
+	bn: "এই পেইজটির বাংলা সংস্করণ নেই ।",
 	ja: "このページはご利用の言語でご覧いただけません。",
 	"zh-cn": "此页面不支持您的语言。",
 	ar: "هذه الصفحة غير متوفرة بلغتك.",
