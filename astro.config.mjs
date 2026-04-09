0c02e0398ae7ff9ede0bed37d990cb96c37a255e
safin-sys <mdsafinofficial@gmail.com>
2026-04-10 00:21:26 +0600

    url changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from './src/locales';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://grief-stricken-winds.com', // Set your site's URL
+  site: 'https://grief-stricken-winds.vercel.app', // Set your site's URL
   i18n: {
     defaultLocale: DEFAULT_LOCALE_SETTING,
     locales: Object.keys(LOCALES_SETTING),
